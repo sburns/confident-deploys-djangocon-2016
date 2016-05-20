6069a761b30ce6c0f50ec5ce49e83c45b5c106da
Scott Burns <scott.s.burns@gmail.com>
2016-05-20 10:46:16 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,28 @@
+# Confident Asset Deployments with Webpack & Django
+
+This is my 2016 talk for DjangoCon. Probably not much to see now.
+
+## Colophon
+
+This presentation is built with [Spectacle](https://github.com/FormidableLabs/spectacle) using the [boilerplate](https://github.com/FormidableLabs/spectacle-boilerplate).
+
+### Reference
+
+The Spectacle core API is available at [https://github.com/FormidableLabs/spectacle/blob/master/README.markdown](https://github.com/FormidableLabs/spectacle/blob/master/README.markdown).
+
+### Development
+
+After cloning this repo, your first order of business is to open terminal and run `npm install`
+
+Then, to start up the local server, run `npm start`
+
+Open a browser and hit [http://localhost:3000](http://localhost:3000) to view the presentation.
+
+### Build & Deployment
+
+Building the dist version of the project is as easy as running `npm run build`
+
+If you want to deploy the slideshow to surge, run `npm run deploy`
+
+TODO: Link to a deployed version.
+
---
+++ presentation/index.js
@@ -0,0 +1,70 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Import custom component
+import Interactive from "../assets/interactive";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+
+const images = {
+  city: require("../assets/city.jpg"),
+  kat: require("../assets/kat.png"),
+  logo: require("../assets/formidable-logo.svg"),
+  markdown: require("../assets/markdown.png")
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: "#ff4081"
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Spectacle theme={theme}>
+        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+          <Slide transition={["zoom"]} bgColor="primary">
+            <Heading size={1}  caps lineHeight={1} textColor="black">
+              Confident Asset Deployments
+              with Webpack & Django
+            </Heading>
+            <Heading size={0.5}  caps>
+              Scott Burns
+            </Heading>
+          </Slide>
+        </Deck>
+      </Spectacle>
+    );
+  }
+}
